69df84504201ab3548eb311e05599b70c20f466b
Aruna Turay <tura_arun@yahoo.com>
2023-05-01 15:01:25 -0400

    liker app

--- main.js
+++ main.js
@@ -1,10 +1,66 @@
 // Defining text characters for the empty and full hearts for you to use later.
+console.log("js loading...")
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorMod = document.getElementById('modal') //access error modal
+errorMod.classList.add('hidden') //add class to error mod
+
+//creates events listeners for click events
+const heartClicked = (e) => { //creates event for the just the hearts
+  mimicServerCall() //mimicServer invoked
+  .then(() => {  //when the server responds
+    if(e.target.textContent == EMPTY_HEART) { //checks heart if empty
+      e.target.textContent = FULL_HEART  // change it full heart
+      e.target.classList.add('activated-heart') //added class gives color to full heart
+      }else {
+        e.target.textContent = EMPTY_HEART // other option is to change to empty heart
+        e.target.classList.remove('activated-heart') //and then remove the color
+      }
+  })
+  .catch(() => { // if a negative response is rendered
+    errorMod.classList.remove('hidden') // to display error message
+    setTimeout(function() { //timeour to make the error dissappear after 3 seconds
+      errorMod.classList.add('hidden')
+    }, 3000)
+  })
+}
+
+let hearts = document.getElementsByClassName('like-glyph') //to access all hearts
+for(let heart of hearts) { //a for..Of loop to iterate through the hearts collection
+  heart.addEventListener('click', heartClicked) //adds an event listener to the heart
+}
 
 
+
+//my failing code needs further investigation
+/*
+function addingEventListeners () {
+  let likes = document.querySelectorAll('li')
+  for(let like of likes)
+  like.addEventListener('click', handleClick)
+}
+addingEventListeners()
+
+function handleClick(e) {
+  mimicServerCall()
+       .then()
+       .catch((error) => {error})
+     if(mimicServerCall === false) {
+        setTimeout(() => {
+          hide = docuemnt.getElementById('modal')
+          hide.classList.add(".hidden")
+          }, 3000)
+        e.target.textContent = EMPTY_HEART;
+      } else {
+        e.target.textContent = FULL_HEART;
+        show = document.querySelector('.hidden')
+        show.className.textContent = (".activated-heart")
+      }
+    } 
+
+*/
 
 
 //------------------------------------------------------------------------------
---
+++ planning.md
@@ -0,0 +1,16 @@
+Stage 1
+[] create a click event when the user clicks on the heart symbol
+[] add a catch() method when that catches a failed response
+[] Display the failed message in the modal by removing the .hidden class
+
+Stagee 2
+Server Return failure
+[] Make the modal dissappear aft3er 3 seconds by using setTimeout
+[] add the hidden class
+[]
+
+Stage 3
+Server returns success
+[] change the heart to full heart by adding .activated-heart class 
+[] change the heart back to empty heart
+[] remove the .activated-heart to make the heart empty again
